e41c4d2a523175b125dc9b4988a6d9b052e67c34
ls-95 <143622355+ls-95@users.noreply.github.com>
2025-05-15 11:32:01 +0200

    Initial commit - Add generate qr code from input value

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import QrCodeGenerator from "./components/QrCodeGenerator";
+
+function App() {
+  return (
+    <div className="App">
+      <QrCodeGenerator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/QrCodeGenerator.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import QRCode from "react-qr-code";
+
+export default function QrCode() {
+  const [qrCode, setQrCode] = useState("");
+  const [input, setInput] = useState("");
+
+  function handleGenerateQrCode() {
+    setQrCode(input);
+  }
+
+  return (
+    <div>
+      <h1>Qr Code Generator</h1>
+      <div>
+        <input
+          type="text"
+          name="qr-code"
+          placeholder="Enter your value here"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button
+          disabled={input && input.trim() !== "" ? false : true}
+          onClick={handleGenerateQrCode}
+        >
+          Generate
+        </button>
+        <div>
+          <QRCode id="qr-code-value" value={qrCode} size={400} bgColor="#fff" />
+        </div>
+      </div>
+    </div>
+  );
+}
